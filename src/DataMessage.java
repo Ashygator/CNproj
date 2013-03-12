4c78f83536723c09d324adc1e4e1e864f1029814
AshyGator <ashish.g.bhat@gmail.com>
2013-03-11 21:44:02 -0400

    THIRD commit

---
+++ src/DataMessage.java
@@ -0,0 +1,15 @@
+
+public class DataMessage implements RequiredConstants {
+	
+	private byte[] len = null;
+	private byte[] type = null;
+	private byte[] payload = null;
+	private String messageLength;
+	private String messageType;
+	private int dataLength = DATA_MESSAGE_TYPE;
+
+	public DataMessage()
+	{
+		
+	}
+}
